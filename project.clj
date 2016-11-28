432c1c0bf3f214c4a65647b80b45d51291d18a58
Christopher Mark Gore <cgore@cgore.com>
2016-11-28 13:29:32 -0600

    Updating project description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject yestermorrow "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description " Yestermorrow is a library of additional time-related functions for Clojure using clj-time."
+  :url "https://github.com/cgore/yestermorrow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
