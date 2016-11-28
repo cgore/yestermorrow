7929dbd7aec8224f413d696ebc8f85a97ed25a99
Christopher Mark Gore <cgore@cgore.com>
2016-11-28 13:40:09 -0600

    Adding date-noon and local-date-noon

--- src/yestermorrow/core.clj
+++ src/yestermorrow/core.clj
@@ -1,2 +1,26 @@
-(ns yestermorrow.core)
+(ns yestermorrow.core
+  (:require [clj-time.core :refer :all]))
 
+(defn date-noon
+  "Constructs and returns a new LocalDateTime at noon.
+   Specify the year, month of year, day of month. Note that month and day are
+   1-indexed. Any number of least-significant components can be ommited, in which case
+   they will default to 1."
+  ([year]
+   (date-noon year 1 1))
+  ([year month]
+   (date-noon year month 1))
+  ([year month day]
+   (date-time year month day 12)))
+
+(defn local-date-noon
+  "Constructs and returns a new DateTime in UTC at noon.
+   Specify the year, month of year, day of month. Note that month and day are
+   1-indexed. Any number of least-significant components can be ommited, in which case
+   they will default to 1."
+  ([year]
+   (local-date-noon year 1 1))
+  ([year month]
+   (local-date-noon year month 1))
+  ([year month day]
+   (local-date-time year month day 12)))
--- test/yestermorrow/core_test.clj
+++ test/yestermorrow/core_test.clj
@@ -1,5 +1,24 @@
 (ns yestermorrow.core-test
   (:require [clojure.test :refer :all]
+            [clj-time.core :refer :all]
             [yestermorrow.core :refer :all]))
 
+(deftest test-date-noon-and-accessors
+  (let [d (date-noon 1986)]
+    (is (= 1986 (year   d)))
+    (is (= 1    (month  d)))
+    (is (= 1    (day    d)))
+    (is (= 12   (hour   d)))
+    (is (= 0    (minute d)))
+    (is (= 0    (second d)))
+    (is (= 0    (milli  d)))))
 
+(deftest test-local-date-noon-and-accessors
+  (let [d (local-date-noon 1986)]
+    (is (= 1986 (year   d)))
+    (is (= 1    (month  d)))
+    (is (= 1    (day    d)))
+    (is (= 12   (hour   d)))
+    (is (= 0    (minute d)))
+    (is (= 0    (second d)))
+    (is (= 0    (milli  d)))))
